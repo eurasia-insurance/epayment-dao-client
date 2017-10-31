1c1cdcc02d484b36896747be0d0f59b5f1f1aba8
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-31 18:36:01 +0600

    old daos

--- client/src/main/java/tech/lapsa/epayment/dao/KKBDocumentDAO.java
+++
@@ -1,9 +0,0 @@
-package tech.lapsa.epayment.dao;
-
-import javax.ejb.Local;
-
-import com.lapsa.kkb.core.KKBDocument;
-
-@Local
-public interface KKBDocumentDAO extends KKBGeneralDocumentDAO<KKBDocument> {
-}
