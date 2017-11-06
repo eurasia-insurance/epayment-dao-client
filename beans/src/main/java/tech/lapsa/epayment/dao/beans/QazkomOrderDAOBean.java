618daedd9bfed7422369b2494e073df6b3c79a87
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-06 14:33:35 +0600

    get by orderNumber

--- beans/src/main/java/tech/lapsa/epayment/dao/beans/QazkomOrderDAOBean.java
+++ beans/src/main/java/tech/lapsa/epayment/dao/beans/QazkomOrderDAOBean.java
@@ -1,14 +1,39 @@
 package tech.lapsa.epayment.dao.beans;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import tech.lapsa.epayment.dao.QazkomOrderDAO;
 import tech.lapsa.epayment.domain.QazkomOrder;
+import tech.lapsa.epayment.domain.QazkomOrder_;
+import tech.lapsa.java.commons.function.MyStrings;
+import tech.lapsa.patterns.dao.NotFound;
+import tech.lapsa.patterns.dao.TooMuchFound;
 
 @Stateless
 public class QazkomOrderDAOBean extends ABaseDAO<QazkomOrder, Integer> implements QazkomOrderDAO {
 
     public QazkomOrderDAOBean() {
 	super(QazkomOrder.class);
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    public QazkomOrder getByNumber(String number) throws NotFound, IllegalArgumentException, TooMuchFound {
+	MyStrings.requireNonEmpty(number, "number");
+
+	CriteriaBuilder cb = em.getCriteriaBuilder();
+	CriteriaQuery<QazkomOrder> cq = cb.createQuery(QazkomOrder.class);
+	Root<QazkomOrder> root = cq.from(QazkomOrder.class);
+	cq.select(root) //
+		.where(cb.equal(root.get(QazkomOrder_.orderNumber), number));
+
+	TypedQuery<QazkomOrder> q = em.createQuery(cq);
+	return signleResultNoCached(q);
     }
 }
--- beans/src/main/java/tech/lapsa/epayment/dao/beans/QazkomPaymentDAOBean.java
+++ beans/src/main/java/tech/lapsa/epayment/dao/beans/QazkomPaymentDAOBean.java
@@ -1,14 +1,39 @@
 package tech.lapsa.epayment.dao.beans;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import tech.lapsa.epayment.dao.QazkomPaymentDAO;
 import tech.lapsa.epayment.domain.QazkomPayment;
+import tech.lapsa.epayment.domain.QazkomPayment_;
+import tech.lapsa.java.commons.function.MyStrings;
+import tech.lapsa.patterns.dao.NotFound;
+import tech.lapsa.patterns.dao.TooMuchFound;
 
 @Stateless
 public class QazkomPaymentDAOBean extends ABaseDAO<QazkomPayment, Integer> implements QazkomPaymentDAO {
 
     public QazkomPaymentDAOBean() {
 	super(QazkomPayment.class);
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    public QazkomPayment getByNumber(String number) throws NotFound, IllegalArgumentException, TooMuchFound {
+	MyStrings.requireNonEmpty(number, "number");
+
+	CriteriaBuilder cb = em.getCriteriaBuilder();
+	CriteriaQuery<QazkomPayment> cq = cb.createQuery(QazkomPayment.class);
+	Root<QazkomPayment> root = cq.from(QazkomPayment.class);
+	cq.select(root) //
+		.where(cb.equal(root.get(QazkomPayment_.orderNumber), number));
+
+	TypedQuery<QazkomPayment> q = em.createQuery(cq);
+	return signleResultNoCached(q);
     }
 }
