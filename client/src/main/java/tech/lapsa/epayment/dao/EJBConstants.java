55c2bf4be65c4fc3d4becab80bd7cfa88399af29
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-16 10:05:14 +0600

    constant renamed

--- client/src/main/java/tech/lapsa/epayment/dao/EJBConstants.java
+++ client/src/main/java/tech/lapsa/epayment/dao/EJBConstants.java
@@ -2,6 +2,6 @@ package tech.lapsa.epayment.dao;
 
 public interface EJBConstants {
 
-    public static final String APPLICATION_NAME = "epayment-dao-ear";
+    public static final String REMOTE_APP_NAME = "epayment-dao-ear";
     public static final String MODULE_NAME = "epayment-dao";
 }
